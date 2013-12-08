cbbb61b239914d5413c1dd5a09358fd039dc2981
Anders Malmborg <anders.malmborg@porscheinformatik.at>
2013-12-08 18:56:47 +0100

    Not working Camel/ActiveMQ example.

--- events-camel/src/test/java/events/camel/services/StockServiceTest.java
+++ events-camel/src/test/java/events/camel/services/StockServiceTest.java
@@ -1,7 +1,5 @@
 package events.camel.services;
 
-import javax.persistence.OptimisticLockException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
